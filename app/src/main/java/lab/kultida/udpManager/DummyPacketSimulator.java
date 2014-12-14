a79c4dfc22216f9e4917ea80465caa442fe6783f
AsusPC <libertywing002@gmail.com>
2014-12-14 20:12:47 +0700

    WTF  งงโว๊ยยยยยยยยย

---
+++ app/src/main/java/lab/kultida/udpManager/DummyPacketSimulator.java
@@ -0,0 +1,162 @@
+package lab.kultida.udpManager;
+
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.conn.util.InetAddressUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Collections;
+import java.util.List;
+
+import lab.kultida.seniorproject.Prototype;
+import lab.kultida.seniorproject.R;
+
+public class DummyPacketSimulator extends Prototype {
+
+	protected EditText editText_IpAddress;
+	protected EditText editText_Port;
+	protected EditText editText_MessageSize;
+	protected EditText editText_LoopCount;
+	protected EditText editText_Delay;
+	protected TextView textView_DeviceInfo;
+	protected Button button_SendMessage;
+	protected TextView textView_Status;
+	protected int port_Client = 33193;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+        super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_dummy_packet_simulator);
+		getComponent();
+		
+		//test
+		editText_IpAddress.setText("192.168.248.1");
+		editText_Port.setText("6664");
+		editText_MessageSize.setText("");
+		editText_LoopCount.setText("10");
+		editText_Delay.setText("2000");
+	}
+	
+	protected void getComponent(){
+		editText_IpAddress = (EditText) findViewById(R.id.editText_IpAddress);
+		editText_Port = (EditText) findViewById(R.id.editText_Port);
+		editText_MessageSize = (EditText) findViewById(R.id.editText_MessageSize);
+		editText_LoopCount = (EditText) findViewById(R.id.editText_LoopCount);
+		editText_Delay = (EditText) findViewById(R.id.editText_Delay);
+		textView_DeviceInfo = (TextView) findViewById(R.id.textView_DeviceInfo);
+		button_SendMessage = (Button) findViewById(R.id.button_SendMessage);
+		textView_Status = (TextView) findViewById(R.id.textView_Status);
+		textView_Status.setMovementMethod(new ScrollingMovementMethod());
+	}
+
+	public void sendMessage(View view){
+		clearTextfield();
+		setProgressBarIndeterminateVisibility(true);
+		Log.d("sendMessage", "ready to send Message");
+		Toast.makeText(DummyPacketSimulator.this, "Ready to send Message", Toast.LENGTH_SHORT).show();
+		
+		try {
+			//dont forget check parameter is correct
+			int loop = 1;
+			if(!editText_LoopCount.getText().toString().matches("")) loop = Integer.parseInt(editText_LoopCount.getText().toString());
+
+            textView_DeviceInfo.setText("Device Information \nIP Address : " + getIPAddress(true) + "\nClient Port : " + port_Client);
+			
+			for(int i = 0;i < loop;i++){
+				JSONObject connectionInfo = getJSONValue(i + 1);
+				UDPClient udpclient = new UDPClient(connectionInfo,textView_DeviceInfo,textView_Status);
+				if(editText_MessageSize.getText().toString().matches("")) {
+					udpclient.sendMessage("test");
+				}else{
+					udpclient.sendMessage(Integer.parseInt(editText_MessageSize.getText().toString()));
+				}
+			}
+			textView_DeviceInfo.append("Finished");
+			
+		} catch (Exception e) {
+			Log.d("DummyPacketSimulator", "Error to Execute");
+			e.printStackTrace();
+		}
+
+		setProgressBarIndeterminateVisibility(false);
+	}
+	
+	public JSONObject getJSONValue(int loop){
+		JSONObject connectionInfo = new JSONObject();
+		
+		if(!editText_IpAddress.getText().toString().matches(""))
+			try {
+				connectionInfo.put("serverIp", editText_IpAddress.getText().toString());
+				if(!editText_Port.getText().toString().matches("")) connectionInfo.put("serverPort", editText_Port.getText().toString());
+				connectionInfo.put("clientPort", port_Client);
+				if(!editText_MessageSize.getText().toString().matches("")) connectionInfo.put("size", editText_MessageSize.getText().toString());
+				connectionInfo.put("loop", loop);
+				if(!editText_Delay.getText().toString().matches("")) connectionInfo.put("delay", editText_Delay.getText().toString());
+					
+				//check ip has . 3 times
+				//check port size loop delay is + integer only 
+			} catch (JSONException e) {
+				Log.d("DummyPacketSimulator - getJSONValue", "Input Data Invalid");
+				e.printStackTrace();
+			}
+		
+		return connectionInfo;
+	}
+	
+	public static String getIPAddress(boolean useIPv4) {
+        // useIPv4 = true  >> IPv4
+		//  	   = false >> IPv6
+		try {
+            List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface intf : interfaces) {
+                List<InetAddress> addrs = Collections.list(intf.getInetAddresses());
+                for (InetAddress addr : addrs) {
+                    if (!addr.isLoopbackAddress()) {
+                        String sAddr = addr.getHostAddress().toUpperCase();
+                        boolean isIPv4 = InetAddressUtils.isIPv4Address(sAddr);
+                        if (useIPv4) {
+                            if (isIPv4) 
+                                return sAddr;
+                        } else {
+                            if (!isIPv4) {
+                                int delim = sAddr.indexOf('%'); // drop ip6 port suffix
+                                return delim<0 ? sAddr : sAddr.substring(0, delim);
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception ex) { } // for now eat exceptions
+        return "";
+    }
+
+	public void clearTextfield(){
+		textView_DeviceInfo.setText("");
+		textView_Status.setText("");
+	}
+	
+	public void clearEditText(){
+		editText_IpAddress.setText("");
+		editText_Port.setText("");
+		editText_MessageSize.setText("");
+		editText_LoopCount.setText("");
+		editText_Delay.setText("");
+	}
+
+	public void clearScreen(View view){
+		clearEditText();
+		clearTextfield();
+	}
+}
---
+++ app/src/main/java/lab/kultida/udpManager/UDPClient.java
@@ -0,0 +1,180 @@
+package lab.kultida.udpManager;
+
+import android.util.Log;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+public class UDPClient implements Runnable {
+	public final int DEFAULT_SERVER_PORT = 50003; 
+	public final int DEFAULT_CLIENT_PORT = 33193;
+	public final int DEFAULT_BCAST_PORT = 8779;
+
+	private InetAddress serverIp;
+	private int serverPort;
+	private int clientPort;
+	private int loop;
+	private int delay;
+	
+	private String messageShow;
+	private byte[] bytes;
+	private DatagramSocket socket;
+	
+	private TextView textView_Status;
+	
+	private boolean isRunning;
+	private Thread thread;
+	
+	public UDPClient(JSONObject connectionInfo,TextView textView_DeviceInfo,TextView textView_Status) {
+		try {
+			if(connectionInfo.has("serverIp")) this.serverIp = InetAddress.getByName(connectionInfo.getString("serverIp"));
+			else this.serverIp = InetAddress.getByName("127.0.0.1");
+			
+			if(connectionInfo.has("serverPort")) this.serverPort = Integer.parseInt(connectionInfo.getString("serverPort"));
+			else this.serverPort = DEFAULT_SERVER_PORT;
+			
+			if(connectionInfo.has("clientPort")) this.clientPort = Integer.parseInt(connectionInfo.getString("clientPort"));
+			else this.clientPort = DEFAULT_CLIENT_PORT;
+			
+			if(connectionInfo.has("loop")) this.loop = Integer.parseInt(connectionInfo.getString("loop"));
+			else this.loop = 1;
+			
+			if(connectionInfo.has("delay")) this.delay = Integer.parseInt(connectionInfo.getString("delay"));
+			else this.delay = 0;
+			
+			this.textView_Status = textView_Status;
+			
+			Log.d("UDPClient - Constructor", "serverIp = " + serverIp.toString());
+			Log.d("UDPClient - Constructor", "serverPort = " + serverPort);
+			Log.d("UDPClient - Constructor", "clientPort = " + clientPort);
+			Log.d("UDPClient - Constructor", "loop = " + loop);
+			Log.d("UDPClient - Constructor", "delay = " + delay);
+			
+		} catch (JSONException e1) {
+			Log.d("UDPClient - Constructor", "Error - JSONObject ");
+			e1.printStackTrace();
+		} catch (UnknownHostException e) {
+			Log.d("UDPClient - Constructor", "Error Unknow Host Object");
+			e.printStackTrace();
+		}
+		this.isRunning = false;
+		
+		try {
+//			socket = new DatagramSocket(null); 
+//			SocketAddress socketAddr=new InetSocketAddress(DEFAULT_BCAST_PORT); 
+//			socket.setReuseAddress(true); 
+//			socket.setBroadcast(true); 
+//			socket.bind(socketAddr); 
+			
+//			socket = new DatagramSocket(clientPort);
+			socket = new DatagramSocket();
+			socket.setSoTimeout(300);
+//			socket.setBroadcast(true); //why set broadcast
+		} catch (SocketException e) {
+			Log.d("UDPClient - Constructor", "Error Socket: " + e.getMessage());
+			return; 
+		}
+		Log.d("UDPClient - Constructor", "Client created");
+	}
+
+	public void run() { 
+		Log.d("UDPClient - run", "Run");
+		this.isRunning = true;
+//		Log.d("UDPClient - run", "Ready to access loop while");
+//		Log.d("UDPClient - run", "Server ip = " + serverIp);
+//		Log.d("UDPClient - run", "Loop");
+		byte[] sendData = bytes;
+		try {
+			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverIp, serverPort);
+			socket.send(sendPacket);
+			Log.d("UDPClient - run", "Package Sent Complete");
+			textView_Status.append("\nloop : " + loop + "\nPort : " + clientPort + "\nMessage : " + messageShow + "\nByte : " + bytes + "\n");
+		} catch (Exception e) {
+			Log.d("UDPClient - run", "Exception While Thread");
+		}	
+//		receiveMessage();
+		if (socket != null) socket.close();
+		this.isRunning = false;
+	}
+ 
+	public void sendMessage(byte[] bytes) {
+		Log.d("UDPClient - sendMessage", "BYTE = " + bytes);
+		try {
+			if(this.isRunning) {
+				while(this.thread.isAlive());	
+				this.isRunning = false;
+			}
+			this.bytes = bytes;
+			thread = new Thread(this, "Client Thread");
+			thread.start();
+		} catch (Exception e) {
+			Log.d("UDPClient - sendMessage", "Error New Thread");
+			return; 
+		}
+	}
+	
+	public String sendMessage(String message) {
+		messageShow = message;	
+		try {
+			bytes = message.getBytes("UTF-8");
+//				bytes = Serializer.serialize(message);
+		} catch (IOException e) {
+			Log.d("UDPClient - sendMessage", "Error serializer message");
+			e.printStackTrace();
+		}
+		Log.d("UDPClient - sendMessage - String", "message = " + message);
+		sendMessage(bytes);
+		return message;
+	}
+	
+	public void sendMessage(JSONObject message){
+		byte[] bytes = null;
+		try {
+			bytes = Serializer.serialize(message);
+		} catch (IOException e) {
+			Log.d("UDPClient - sendMessage", "Error serializer JSONObject");
+			e.printStackTrace();
+		}
+		sendMessage(bytes);
+	}
+	
+	public String sendMessage(int length){
+		String message = "";
+		for(int i = 0;i < length;i++) message += "0";
+		Log.d("UDPClient - sendMessage", "Message = " + message);
+		return sendMessage(message);
+	}
+
+	public void receiveMessage(){
+		byte[] buffer = new byte[3000]; // set window size
+		Log.d("UDPClient - receiveMessage", "set replyPacket");
+		DatagramPacket replyPacket = new DatagramPacket(buffer, buffer.length); // create buffer for receiving reply
+		Log.d("UDPClient - receiveMessage", "set receive replyPacket");
+		try {
+			socket.receive(replyPacket);
+		} catch (IOException e1) {
+			Log.d("UDPClient - ReceiveMessage", "Error IO");
+			e1.printStackTrace();
+		} // set reply to receive data from aSocket 
+		Log.d("UDPClient - receiveMessage", "receive Socket");
+		String msgReply = null;
+		try {
+			msgReply = (String) Serializer.deserialize(replyPacket.getData()); // change reply message to be byte string
+		} catch (ClassNotFoundException e) {
+			Log.d("UDPClient - receiveMessage", "Error serializer replyPacket"); // print reply message
+			e.printStackTrace();
+		} catch (IOException e) {
+			Log.d("UDPClient - receiveMessage", "Error IO : " + e.getMessage());
+			e.printStackTrace();
+		}
+		Log.d("UDPClient - receiveMessage", "Reply Packet: " + msgReply); // print reply message
+	}
+} 
