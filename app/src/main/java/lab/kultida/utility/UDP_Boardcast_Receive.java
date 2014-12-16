5cbd6312ae83d6e08bad506b7f08cc3c59b282ba
Adsadawut Chanakitkarnchok <Boat_Boat_7@hotmail.com>
2014-12-16 21:52:04 +0700

    Chat Room Finished

--- app/src/main/java/lab/kultida/utility/UDP_Boardcast_Receive.java
+++ app/src/main/java/lab/kultida/utility/UDP_Boardcast_Receive.java
@@ -3,10 +3,15 @@ package lab.kultida.utility;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import org.apache.http.conn.util.InetAddressUtils;
 import org.json.JSONObject;
 
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by F0l2g3tm3n0t on 16-Dec-14.
@@ -37,10 +42,16 @@ public class UDP_Boardcast_Receive extends AsyncTask<String, Void, String> {
 		        Log.d("Receive", "Socket connected");
 		        socket.receive(packet);
 		        String msg = new String(packet.getData());
-		        Log.d("Receive - message", msg);
-		        socket.close();
-		        Log.d("Receive", "socket closed");
-		        return msg;
+		        InetAddress address = packet.getAddress();
+		        InetAddress myAddress = InetAddress.getByName(getIPAddress(true));
+		        if(myAddress == address) {
+			        Log.d("Receive - message", msg);
+			        socket.close();
+			        Log.d("Receive", "socket closed");
+			        return msg;
+		        } else{
+			        return "Fail!!";
+		        }
 	        }
         }catch (Exception e){
             Log.d("Exception Error : ",e.getMessage());
@@ -55,4 +66,30 @@ public class UDP_Boardcast_Receive extends AsyncTask<String, Void, String> {
     @Override
     protected void onPostExecute(String result) {}
 
+	protected String getIPAddress(boolean useIPv4) {
+		// useIPv4 = true  >> IPv4
+		//  	   = false >> IPv6
+		try {
+			List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
+			for (NetworkInterface intf : interfaces) {
+				List<InetAddress> addrs = Collections.list(intf.getInetAddresses());
+				for (InetAddress addr : addrs) {
+					if (!addr.isLoopbackAddress()) {
+						String sAddr = addr.getHostAddress().toUpperCase();
+						boolean isIPv4 = InetAddressUtils.isIPv4Address(sAddr);
+						if (useIPv4) {
+							if (isIPv4)
+								return sAddr;
+						} else {
+							if (!isIPv4) {
+								int delim = sAddr.indexOf('%'); // drop ip6 port suffix
+								return delim<0 ? sAddr : sAddr.substring(0, delim);
+							}
+						}
+					}
+				}
+			}
+		} catch (Exception ex) { } // for now eat exceptions
+		return "";
+	}
 }
