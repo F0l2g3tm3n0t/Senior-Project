fdde07e76eb995439c2e2c7c95005bd635b78bde
AsusPC <libertywing002@gmail.com>
2014-12-21 23:15:35 +0700

    fix tcp unicast

--- app/src/main/java/lab/kultida/utility/TCP_Unicast_Send.java
+++ app/src/main/java/lab/kultida/utility/TCP_Unicast_Send.java
@@ -5,6 +5,7 @@ import android.util.Log;
 
 import org.json.JSONObject;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.InetAddress;
@@ -13,45 +14,53 @@ import java.net.Socket;
 public class TCP_Unicast_Send extends AsyncTask<String, Void, String> {
 
     protected String log_Head = "";
+    protected String data_receive = "";
+    protected boolean receiveData = false;
 
-	@Override
-	protected void onPreExecute() {
+    @Override
+    protected void onPreExecute() {
         // log_Head
-		super.onPreExecute();
-	}
+        super.onPreExecute();
+    }
 
     protected void sleep(){
 
     }
 
-	@Override
-	protected String doInBackground(String... arg0) {
+    @Override
+    protected String doInBackground(String... arg0) {
         Socket socket = null;
         DataOutputStream output = null;
-		try{
-			// Initial condition
+        try{
+            // Initial condition
             JSONObject data_frame = new JSONObject(arg0[0]);
             Log.d(log_Head + " - doInBackground","data_frame : " + data_frame);
             InetAddress serverIP = InetAddress.getByName(data_frame.getString("serverIP"));
-			int serverPort = Integer.parseInt(data_frame.getString("serverPort"));
+            int serverPort = Integer.parseInt(data_frame.getString("serverPort_CheckHotspotInformation"));
             String data = data_frame.getJSONObject("data").toString();
             Log.d(log_Head + " - doInBackground","serverIP : " + data_frame.getString("serverIP"));
-            Log.d(log_Head + " - doInBackground","serverPort : " + serverPort);
+            Log.d(log_Head + " - doInBackground","serverPort_CheckHotspotInformation : " + serverPort);
             Log.d(log_Head + " - doInBackground","send data : " + data);
 
             // open socket
             Log.d(log_Head + " - doInBackground","open socket");
-			socket = new Socket(serverIP,serverPort);
-			output = new DataOutputStream(socket.getOutputStream());
-			output.writeUTF(data);
-			Log.d(log_Head + " - doInBackground", "receive data : " + data);
-			output.flush();
-			output.close();
-			socket.close();
+            socket = new Socket(serverIP,serverPort);
+            output = new DataOutputStream(socket.getOutputStream());
+            output.writeUTF(data);
+            Log.d(log_Head + " - doInBackground", "receive data : " + data);
+            output.flush();
+            output.close();
+            socket.close();
+
+            if(!receiveData){
+                DataInputStream input = new DataInputStream(socket.getInputStream());
+                data_receive = input.readUTF();
+            }
+
             sleep();
-			return "Success : " + data;
-		}catch (Exception e){
-			e.printStackTrace();
+            return "Success : " + data;
+        }catch (Exception e){
+            e.printStackTrace();
             try {
                 if(output != null) output.close();
                 if(socket != null) socket.close();
@@ -60,10 +69,10 @@ public class TCP_Unicast_Send extends AsyncTask<String, Void, String> {
             }
             sleep();
             return "Fail";
-		}
-	}
+        }
+    }
 
-	@Override
-	protected void onPostExecute(String result) {}
+    @Override
+    protected void onPostExecute(String result) {}
 
 }
